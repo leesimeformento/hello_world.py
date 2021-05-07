4bca20eab067bc981c9526771547676ed1121381
Lee Formento <leesimeformento@gmail.com>
2021-05-07 02:19:51 -0500

    done with project

--- TEST.py
+++ TEST.py
@@ -2,6 +2,19 @@
 # for i in range(len(my_list)):
 #     print(my_list[i])
 
-my_dict = {"name": "Lee", "language": "Python"}
-for k in my_dict
-print(my_dict[k])
+
+# succesful = True
+# for number in range(3):
+#     print("Attempt")
+#     if succesful:
+#         print("Succesful")
+#         break
+#     else:
+#         print("attempted 3 times, no luck!!")
+
+numbers = [7, 13, 8, 42]
+for x in range(0, len(numbers)):
+    print(x)
+    print(numbers[x])
+if numbers[len(numbers) - 1] == 42:
+    print("boooo")
